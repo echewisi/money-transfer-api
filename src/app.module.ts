718b88e63aa4cfa3699bee8063b4f59dd38d59d2
Joseph Echewisi <josephechewisi@gmail.com>
2024-10-22 19:34:03 +0100

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { RedisConfigModule } from './database/redis.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module'; // DatabaseModule import
+import { AuthModule } from './authentication/auth.module'; // AuthModule import
+import { UsersModule } from './users/users.module'; // UsersModule import
+import { TransfersModule } from './transfers/transfer.module'; // TransfersModule import
 
 @Module({
-  imports: [],
+  imports: [
+    RedisConfigModule, //redis config module
+    DatabaseModule, // Database for TypeORM
+    AuthModule, // Authentication module
+    UsersModule, // Users-related logic module
+    TransfersModule, // Transfers-related logic module
+    CacheModule.register(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/redis.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: configService.get('REDIS_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [RedisModule],
+})
+export class RedisConfigModule {}
